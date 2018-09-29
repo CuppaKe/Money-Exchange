6cc6d90c454d09d754248342472717b0cecbdfce
CuppaKe <turnman87@gmail.com>
2018-09-29 19:31:02 +0300

    updated syntax and comments

--- src/index.js
+++ src/index.js
@@ -1,36 +1,33 @@
-// PLEASE DON'T change function name
+/**
+ * Function determine the minimum number of coins to exchange currency
+ * @param {number} currency given currency
+ * @return {object} minimum number of coins or error message
+ */
 module.exports = function makeExchange(currency) {
-	
-	let coins = {};
-	let rich = {error:"You are rich, my friend! We don't have so much coins for exchange"}
-
-    if (currency > 10000) return rich;
-    if (currency <= 0) return coins;
-    if (currency >= 50){
-
-    	coins.H = Math.floor(currency/50)
-    	currency = currency - coins.H*50;
-    }
-    if (currency >= 25){
-
-    	coins.Q = Math.floor(currency/25);
-    	currency = currency - coins.Q*25;
-    }
-    if (currency >= 10){
-
-    	coins.D = Math.floor(currency/10);
-    	currency = currency - coins.D*10;
-    }
-    if (currency >= 5){
-
-    	coins.N = Math.floor(currency/5);
-    	currency = currency - coins.N*5;
-
-    }
-    if (currency >= 1){
-
-    	coins.P = Math.floor(currency/1);
-    }
-    return coins;
-   	
-}
+  const coins = {};
+  const rich = { error: "You are rich, my friend! We don't have so much coins for exchange" };
+  let cash = currency;
+  
+  if (cash > 10000) return rich;
+  if (cash <= 0) return coins;
+  if (cash >= 50) {
+    coins.H = Math.floor(cash / 50);
+    cash -= coins.H * 50;
+  }
+  if (cash >= 25) {
+    coins.Q = Math.floor(cash / 25);
+    cash -= coins.Q * 25;
+  }
+  if (cash >= 10) {
+    coins.D = Math.floor(cash / 10);
+    cash -= coins.D * 10;
+  }
+  if (cash >= 5) {
+    coins.N = Math.floor(cash / 5);
+    cash -= coins.N * 5;
+  }
+  if (cash >= 1) {
+    coins.P = Math.floor(cash / 1);
+  }
+  return coins;
+};
